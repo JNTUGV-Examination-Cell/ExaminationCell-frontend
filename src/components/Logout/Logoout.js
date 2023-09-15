c14fc899da5314030fe19377df0dc93258d5d96d
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2023-09-15 21:39:33 +0530

    structure

---
+++ src/components/Logout/Logoout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logoout = () => {
+  return <div>Logoout</div>;
+};
+
+export default Logoout;
---
+++ src/components/PortalNotifications/PortalNotification.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PortalNotification = () => {
+  return <div>PortalNotification</div>;
+};
+
+export default PortalNotification;
