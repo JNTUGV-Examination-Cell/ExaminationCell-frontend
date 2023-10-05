8be470be920ab44c39ad33ddce49d9a3b0215242
Yatheesha20 <badanayatheesha2004@gmail.com>
2023-10-05 21:26:12 +0530

    CondonationandDetention

--- src/Application.js
+++ src/Application.js
@@ -13,6 +13,7 @@ import HomePage from "./components/Layout/LayoutSubPages/HomeSection/HomePage";
 import PortalNotification from "./components/Layout/LayoutSubPages/PortalNotification/PortalNotification";
 import Staff from "./components/Layout/LayoutSubPages/Staff/Staff";
 import Examdata from "./components/Layout/LayoutSubPages/Examination/ExaminationSubPages/Exams/Examdata";
+import CondonationandDetention from "./components/Layout/LayoutSubPages/Examination/ExaminationSubPages/CondonationandDetention/CondonationandDetention";
 const Application = () => {
   return (
     <div>
@@ -27,6 +28,7 @@ const Application = () => {
         />
         <Route path="/admin/batches" element={<Batchesmain />} />
         <Route path="/layout" element={<Layout />}>
+        <Route path="CondonationandDetention" element={<CondonationandDetention />} />
           <Route path="colleges" element={<CollegePage />} />
           <Route path="examdata" element={<Examdata />} />
           <Route path="profilepage" element={<ProfilePage />} />
