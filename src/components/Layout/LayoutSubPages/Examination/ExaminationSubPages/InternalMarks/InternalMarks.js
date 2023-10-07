bfb0e0d505e49094aa660d20c4bf50cc0d020a06
Yatheesha20 <badanayatheesha2004@gmail.com>
2023-10-07 21:14:01 +0530

    Internal Marks Page

--- src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/InternalMarks/InternalMarks.js
+++ src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/InternalMarks/InternalMarks.js
@@ -1,83 +1,197 @@
-import React, { useState } from 'react';
-import './InternalMarks.css'; // Import the CSS file
+import React, { useState, useEffect } from 'react';
+import './InternalMarks.css';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel'; 
+import FormControl from '@mui/material/FormControl'; 
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import TableBody from '@mui/material/TableBody';
+import Paper from '@mui/material/Paper';
+
 
 const jsonData = [
-    { Slno: 1, Hallticket: '22NM1A05B5', Name : 'PALADUGU ANITHA' , Branch : 'CSE' , SubjectCode : 'R201102', Subject : 'COMMUNICATIVE ENGLISH', FinalInternalMarks : 23 },
-    { Slno: 2, Hallticket: '22NM1A04G9', Name : 'YENUMULA VIRAJA SHANMUKHI' , Branch : 'ECE' , SubjectCode : 'R201101', Subject : 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks : 28 },
-    { Slno: 3, Hallticket: '22NM1A04G9', Name : 'YENUMULA VIRAJA SHANMUKHI' , Branch : 'ECE' , SubjectCode : 'R201102', Subject : 'COMMUNICATIVE ENGLISH', FinalInternalMarks : 27 },
-    { Slno: 4, Hallticket: '22NM1A04G8', Name : 'YELLAMELLI ANKITA' , Branch : 'ECE' , SubjectCode : 'R201102', Subject : 'COMMUNICATIVE ENGLISH', FinalInternalMarks : 24 },
-    { Slno: 5, Hallticket: '22NM1A04G6', Name : 'YARIPALLI AMANI' , Branch : 'ECE' , SubjectCode : 'R201115', Subject : 'APPLIED CHEMISTRY', FinalInternalMarks : 25 },
-    { Slno: 6, Hallticket: '22NM1A04G6', Name : 'YARIPALLI AMANI' , Branch : 'ECE' , SubjectCode : 'R201101', Subject : 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks : 26 },
-    { Slno: 7, Hallticket: '22NM1A04G6', Name : 'YARIPALLI AMANI' , Branch : 'ECE' , SubjectCode : 'R201102', Subject : 'COMMUNICATIVE ENGLISH', FinalInternalMarks : 24 },
-    { Slno: 8, Hallticket: '22NM1A04F6', Name : 'VAITLA SUREKHA' , Branch : 'ECE' , SubjectCode : 'R201101', Subject : 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks : 26 },
+  { Slno: 1, Hallticket: '22NM1A05B5', Name: 'PALADUGU ANITHA', Branch: 'CSE', SubjectCode: 'R201102', Subject: 'COMMUNICATIVE ENGLISH', FinalInternalMarks: 23 },
+  { Slno: 2, Hallticket: '22NM1A04G9', Name: 'YENUMULA VIRAJA SHANMUKHI', Branch: 'ECE', SubjectCode: 'R201101', Subject: 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks: 28 },
+  { Slno: 3, Hallticket: '22NM1A04G9', Name: 'YENUMULA VIRAJA SHANMUKHI', Branch: 'ECE', SubjectCode: 'R201102', Subject: 'COMMUNICATIVE ENGLISH', FinalInternalMarks: 27 },
+  { Slno: 4, Hallticket: '22NM1A04G8', Name: 'YELLAMELLI ANKITA', Branch: 'ECE', SubjectCode: 'R201102', Subject: 'COMMUNICATIVE ENGLISH', FinalInternalMarks: 24 },
+  { Slno: 5, Hallticket: '22NM1A04G6', Name: 'YARIPALLI AMANI', Branch: 'ECE', SubjectCode: 'R201115', Subject: 'APPLIED CHEMISTRY', FinalInternalMarks: 25 },
+  { Slno: 6, Hallticket: '22NM1A04G6', Name: 'YARIPALLI AMANI', Branch: 'ECE', SubjectCode: 'R201101', Subject: 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks: 26 },
+  { Slno: 7, Hallticket: '22NM1A04G6', Name: 'YARIPALLI AMANI', Branch: 'ECE', SubjectCode: 'R201102', Subject: 'COMMUNICATIVE ENGLISH', FinalInternalMarks: 24 },
+  { Slno: 8, Hallticket: '22NM1A04F6', Name: 'VAITLA SUREKHA', Branch: 'ECE', SubjectCode: 'R201101', Subject: 'MATHEMATICS-I[CALCULUS]', FinalInternalMarks: 26 },
 ];
 
-function App() {
+function InternalMarks() {
   const [searchQuery, setSearchQuery] = useState('');
+  const [selectedBranch, setSelectedBranch] = useState('All');
+  const [selectedSubject, setSelectedSubject] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+  const dataKey = 'internalMarksData';
 
-  // Function to handle the search
-  const handleSearch = () => {
-    // Perform the search logic here based on the searchQuery state
-    // For example, filter the jsonData based on the searchQuery
-    const filteredData = jsonData.filter((user) =>
-      user.Hallticket.includes(searchQuery) ||
-      user.Name.includes(searchQuery)
+  useEffect(() => {
+    const storedData = localStorage.getItem(dataKey);
+    if (storedData) {
+      setFilteredData(JSON.parse(storedData));
+    } else {
+      setFilteredData(jsonData);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(dataKey, JSON.stringify(filteredData));
+  }, [filteredData]);
+
+  useEffect(() => {
+    
+    let filteredData = jsonData.filter((user) =>
+      
+      (user.Hallticket.includes(searchQuery) ||
+      user.Name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      user.Subject.toLowerCase().includes(searchQuery.toLowerCase())) &&
+      (selectedBranch === 'All' || user.Branch === selectedBranch) 
     );
+    
+    
 
-    // Update the UI or perform any action with the filtered data
-    console.log(filteredData);
+    setFilteredData(filteredData);
+  }, [searchQuery, selectedBranch, selectedSubject]);
+
+  
+  const generateCSV = () => {
+    const header = 'Sl no,Hallticket,Name,Branch,Subject Code,Subject,Final Internal Marks\n';
+    const csvData = filteredData.map((user) =>
+      `${user.Slno},${user.Hallticket},${user.Name},${user.Branch},${user.SubjectCode},${user.Subject},${user.FinalInternalMarks}`
+    );
+    return header + csvData.join('\n');
   };
 
+  
+  const downloadCSV = () => {
+    const csvContent = generateCSV();
+    const blob = new Blob([csvContent], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'internal_marks.csv';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+
+  const handleSearchInputClick = () => {
+    setSelectedSubject('');
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'searchQuery') {
+      setSearchQuery(value);
+    } else if (name === 'selectedSubject') {
+      setSelectedSubject(value);
+    }
+  };
+
+
   return (
-    <div>
-      <div>
-        <h1 className="main-heading">Internal Marks - R111223 - B.Tech I Year I Sem R20 Reg February 2023 </h1>
+    <div className="page-container">
+      <div className="header">
+        <Typography variant="h4" className="main-heading">
+          Internal Marks - R111223 - B.Tech I Year I Sem R20 Reg February 2023
+        </Typography>
       </div>
-      <div className='buttons'>
-        <button className='download-button'>Download Final Internal Marks</button>
+
+      <div className="buttons" style={{paddingTop:'10px',}}>
+        <button className="download-button" onClick={downloadCSV} style={{backgroundColor:'#c0c0c0'}}>
+          <b>
+          Download Final Internal Marks
+          </b>
+        </button>
       </div>
       <center>
-        <h2 className='sub-heading'>List of Students with Final Internal Marks</h2>
+        <Typography variant="h5" className="sub-heading">
+          List of Students with Final Internal Marks
+        </Typography>
       </center>
-      <div className="setline1">
-        <div className='set1'>
-          <button className="search-button" onClick={handleSearch}>Search by Hallticket/Name/MobileNo/Id</button>
-          <div className="search-results"></div>
-        </div>
-        <div className='set2'>
-          <select className="dropdown-select">
-            <option value="option1">Branch</option>
-          </select>
-        </div>
+      <div className='wrap'>
+        <div className='search-container'>
+      <div className="search-bar">
+        <TextField
+          label="Search by Hallticket/Name/Subject"
+          variant="outlined"
+          name="searchQuery"
+          value={searchQuery}
+          onChange={(e) => handleInputChange(e)}
+          fullWidth
+          onClick={handleSearchInputClick}
+        />
       </div>
-      <table className="data-table">
-        <thead>
-          <tr>
-            <th>Sl no</th>
-            <th>Hallticket</th>
-            <th>Name</th>
-            <th>Branch</th>
-            <th>Subject Code</th>
-            <th>Subject</th>
-            <th>Final Internal Marks</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jsonData.map((user) => (
-            <tr key={user.Slno}>
-              <td>{user.Slno}</td>
-              <td>{user.Hallticket}</td>
-              <td>{user.Name}</td>
-              <td>{user.Branch}</td>
-              <td>{user.SubjectCode}</td>
-              <td>{user.Subject}</td>
-              <td>{user.FinalInternalMarks}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="branch-filter">
+  <FormControl variant="outlined" className="branch-select" style={{ width: '200px' }}>
+    <InputLabel htmlFor="branch-select">Filter</InputLabel>
+    <Select
+      label="Branch"
+      value={selectedBranch}
+      onChange={(e) => setSelectedBranch(e.target.value)}
+    >
+      
+      <MenuItem value="All">Select Branch</MenuItem>
+      <MenuItem value="CSE">CSE</MenuItem>
+      <MenuItem value="ECE">ECE</MenuItem>
+    </Select>
+  </FormControl>
+</div>
+
+      </div>
+      </div>
+      <Paper elevation={3} className="table-container" style={{ width: '120%' }}>
+        <Table className="data-table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Sl no</TableCell>
+              <TableCell>Hallticket</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Branch</TableCell>
+              <TableCell>Subject Code</TableCell>
+              <TableCell>Subject</TableCell>
+              <TableCell>Final Internal Marks</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {filteredData.length > 0 ? (
+              filteredData.map((user, index) => (
+                <TableRow
+                  key={user.Slno}
+                  className={`table-row ${index % 2 === 0 ? 'even-row' : ''} hover-row`}
+                >
+                  <TableCell>{user.Slno}</TableCell>
+                  <TableCell>{user.Hallticket}</TableCell>
+                  <TableCell>{user.Name}</TableCell>
+                  <TableCell>{user.Branch}</TableCell>
+                  <TableCell>{user.SubjectCode}</TableCell>
+                  <TableCell>{user.Subject}</TableCell>
+                  <TableCell>{user.FinalInternalMarks}</TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={7}>No matching records found</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </Paper>
     </div>
   );
 }
 
-export default App;
+export default InternalMarks;
+
+
+
