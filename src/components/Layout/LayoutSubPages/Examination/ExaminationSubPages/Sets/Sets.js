f896977f79c50cae580641375341c9651d6a814c
sharmila256 <gsivaramakrishna15@gmail.com>
2023-10-08 15:10:11 +0530

    Sets

--- src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/Sets/Sets.js
+++ src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/Sets/Sets.js
@@ -1,12 +1,16 @@
+// eslint-disable-next-line
 import React, { useState } from 'react';
 import { Typography, Button } from '@mui/material';
-import './Sets.css';
+import { useNavigate } from 'react-router-dom';
+import './Sets.css'; 
 
 const CustomComponent = () => {
-    const [showMessage, setShowMessage] = useState(false);
+    const [college, setCollege] = useState("NM-Vignans Institute of Engineering for women");
+    const navigate = useNavigate();
 
     const handleSettingsClick = () => {
-        setShowMessage(true);
+        setCollege("New College Name"); 
+        navigate('/otherpage');
     }
 
     return (
@@ -15,13 +19,11 @@ const CustomComponent = () => {
                 Exam Center Dashboard - R111223 - B.Tech I Year I sem R20 Reg February 2023
             </Typography>
             <Button variant="contained" onClick={handleSettingsClick}>Sets</Button>
-            {showMessage && (
-                <div className="message">
-                    <Typography variant="h5">
-                        You are hosting this examination for NM-Vignans Institute of Engineering for women
-                    </Typography>
-                </div>
-            )}
+            <div className="message-container">
+                <Typography variant="h5">
+                    You are hosting this examination for {college}
+                </Typography>
+            </div>
         </div>
     )
 } 
