e254af302956ac295a6f8b8c0958aa889e216a30
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2023-10-09 22:17:31 +0530

    code comment fix

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -1,46 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import RollData from "./RoleData";
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-// import LogoutIcon from "@mui/icons-material/Logout";
-// import HomeIcon from "@mui/icons-material/Home";
-// import GroupsIcon from "@mui/icons-material/Groups";
-// import CorporateFareIcon from "@mui/icons-material/CorporateFare";
-// import PersonIcon from "@mui/icons-material/Person";
-// import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import { Typography } from "@mui/material";
 import { selectUserRole } from "../../features/roles/roleSlice";
 import { useSelector } from "react-redux";
 
-const Sidebar = (role) => {
-  // console.log({ role });
-  // const roleLinks = RollData[role.role] || [];
-  // console.log(roleLinks);
+const Sidebar = () => {
   const userRole = useSelector(selectUserRole);
   return (
     <div className="Side-nav">
-      {/* <Link to="/layout/homepage">
-        <HomeIcon className="Nav-icons" />
-        <Typography>Home</Typography>
-      </Link>
-      <Link to="/layout/batches">
-        <GroupsIcon className="Nav-icons" />
-      </Link>
-      <Link to="/layout/colleges">
-        <CorporateFareIcon className="Nav-icons" />
-      </Link>
-      <Link to="/layout/staff">
-        <PersonIcon className="Nav-icons" />
-      </Link>
-      <Link to="/layout/notifications">
-        <NotificationsActiveIcon className="Nav-icons" />
-      </Link>
-      <Link to="/layout/profilepage">
-        <AccountCircleIcon className="Nav-icons" />
-      </Link>
-      <Link to="/">
-        <LogoutIcon className="Nav-icons" />
-      </Link> */}
       {RollData[userRole].map((link, index) => (
         <Link key={index} to={link.to}>
           {link.icon}
