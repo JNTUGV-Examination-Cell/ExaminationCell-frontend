6af0de6ceaa9e7a1af26e6daaa7410e91e99cff0
VamshiGangadhar <vamshi.gangadhar365@gmail.com>
2023-11-02 22:40:52 +0530

    path name updated

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,7 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import "./Layout.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { useSelector } from "react-redux";
 import { selectUserRole } from "../../features/roles/roleSlice";
@@ -9,21 +9,48 @@ import { UserCircle2 } from "lucide-react";
 
 const Layout = (props) => {
   const userRole = useSelector(selectUserRole);
+  const location = useLocation();
+  const pathParts = location.pathname.split("/").filter(Boolean);
   return (
     <Box sx={{ display: "flex" }}>
       <Sidebar />
       <Box component="main" sx={{ flexGrow: 1, pt: "64px" }}>
         {" "}
-        <AppBar position="fixed" sx={{ backgroundColor: "#2E1389" }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            backgroundColor: "#2E1389",
+          }}
+        >
           {" "}
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, color: "#fff", paddingLeft: "250px" }}
-            >
-            JAWAHARLAL NEHRU TECHNOLOGY UNIVERSITY - GURAJADA VIZIANAGARAM
-            </Typography>
+          <Toolbar
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Box>
+              <Typography
+                variant="h6"
+                component="div"
+                sx={{ flexGrow: 1, color: "#fff", paddingLeft: "250px" }}
+              >
+                JAWAHARLAL NEHRU TECHNOLOGY UNIVERSITY - GURAJADA VIZIANAGARAM
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                sx={{ color: "#fff", paddingLeft: "250px" }}
+              >
+                {pathParts.map((part, index) => (
+                  <Link
+                    to={`/${pathParts.slice(0, index + 1).join("/")}`}
+                    style={{ color: "#fff", textDecoration: "none" }}
+                  >
+                    {part}-
+                  </Link>
+                ))}
+              </Typography>
+            </Box>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <UserCircle2 />
               <Typography
