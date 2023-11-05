e44df18e2ca0b53cde1292590f6570a1df76b844
bhuvana205 <badangibhuvana@gmail.com>
2023-11-05 08:40:21 +0530

    SetForParticularExam

--- src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/Sets/SetForParticularExam/SetForParticularExam.js
+++ src/components/Layout/LayoutSubPages/Examination/ExaminationSubPages/Sets/SetForParticularExam/SetForParticularExam.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import "./SetForParticularExam.css";
-import { Typography, Button, Grid } from "@mui/material";
+import { Typography, Button, Grid, Snackbar } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function SetForParticularExam() {
   const setstitle =
     "Sets - R111223 - B.Tech I Year I sem R20 Reg February 2023 - R201102 - COMMUNICATIVE ENGLISH - 20 February 2023 10:00 AM";
   const set = "SET-1";
-  
+
   const [uploadedFile, setUploadedFile] = useState(null);
   const [showDownloadButton, setShowDownloadButton] = useState(false);
+  const [uploadInProgress, setUploadInProgress] = useState(false); 
 
   const handleFileUpload = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
+      setUploadInProgress(true); 
       setUploadedFile(selectedFile);
+      setTimeout(() => {
+        setUploadInProgress(false); 
+      }, 3000); 
     }
   };
 
@@ -102,12 +107,16 @@ function SetForParticularExam() {
         </Typography>
         <hr style={{ width: "50%" }}></hr>
         <Typography variant="h6" className="ntable">
-          {uploadedFile ? "Available" : "Not Available"}
+          {showDownloadButton ? (
+            <span style={{ color: "green" }}>Available</span>
+          ) : (
+            "Not Available"
+          )}
         </Typography>
         <hr style={{ width: "50%" }}></hr>
         <Grid container spacing={2} className="center-buttons">
           <Grid item>
-            <label htmlFor="file-upload" style={{ display: 'block' }}>
+            <label htmlFor="file-upload" style={{ display: "block" }}>
               <Button
                 variant="contained"
                 style={{
@@ -121,7 +130,7 @@ function SetForParticularExam() {
             <input
               type="file"
               id="file-upload"
-              style={{ display: 'none' }}
+              style={{ display: "none" }}
               accept=".pdf"
               onChange={handleFileUpload}
             />
@@ -151,6 +160,11 @@ function SetForParticularExam() {
           </Grid>
         </Grid>
       </div>
+      <Snackbar
+        open={uploadInProgress} 
+        autoHideDuration={3000}
+        message="File upload in progress..."
+      />
     </div>
   );
 }
